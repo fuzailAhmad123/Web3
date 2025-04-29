12ca5b35e264ae35fde06ff66ce48298d815779b
Fuzail Ahmad <fuzailahmad2605@gmail.com>
2025-04-29 12:16:28 +0530

    Update 34_sendEthers.sol

--- SOLIDITY/34_sendEthers.sol
+++ SOLIDITY/34_sendEthers.sol
@@ -37,8 +37,5 @@ contract SendEther{
   }
 
         //recommended and alwasys return bool 
-  function sendViaCall(address payable _to) public payable{
-    ( bool sent, bytes memory data )=  _to.call{ value: msg.value}(""); 
-     require(sent, "failed to send ethers ");
-  }
+ 
 }
